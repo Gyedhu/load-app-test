afbad0d816d14406b8a542e482be8eb2566985fb
gyedhu <gyedhumohanan@gmail.com>
2024-02-14 18:13:53 +0530

    fix: setup tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,12 +3,34 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#6CA89E",
+          "primary-content": "#FFFFFF",
+          secondary: "#eeeeee",
+          "secondary-content": "#121212",
+          accent: "#f7f7f7",
+          "either-content": "#121212",
+          success: "#6BC579",
+          "success-content": "#FFFFFF",
+          warning: "#FBBD23",
+          error: "#E2442F",
+          "error-primary": "#FFFFFF",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ["Saira", ...fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+  ],
+  
 } satisfies Config;
