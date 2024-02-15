23b8494c26d8d56bdc9488370ee2d917566ba077
JEREMIA-SUNNY <you@example.com>
2024-02-15 14:54:10 +0530

    new icon buttons added

---
+++ src/_features/layout/navbar/_components/AddIcon.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { IoMdAdd } from "react-icons/io";
+import IconButton from "~/_components/IconButton";
+
+function AddIcon() {
+  return (
+    <IconButton>
+      <IoMdAdd size={22} />
+    </IconButton>
+  );
+}
+
+export default AddIcon;
--- src/pages/test.dev.tsx
+++ src/pages/test.dev.tsx
@@ -3,15 +3,17 @@ import { NavigationButton } from "~/_features/layout/navbar/_components/Navigati
 import SidebarButton from "~/_features/layout/sidebar/_components/SidebarButton";
 
 import { IoHomeOutline } from "react-icons/io5";
+import IconButton from "~/_components/IconButton";
+import AddIcon from "~/_features/layout/navbar/_components/AddIcon";
+import SignOut from "~/_features/layout/navbar/_components/SignOut";
+import EditIcon from "~/_features/layout/navbar/_components/EditIcon";
 
 const Dev = () => {
   return (
-    <div>
-      <NavigationButton isActive={true}>Trainings </NavigationButton>
-
-      <SidebarButton isExpanded={false} icons={<IoHomeOutline />}>
-        Home
-      </SidebarButton>
+    <div className="h-screen">
+      <div className=" flex h-full items-center  justify-center gap-5">
+        <AddIcon /> <SignOut /> <EditIcon />
+      </div>
     </div>
   );
 };
