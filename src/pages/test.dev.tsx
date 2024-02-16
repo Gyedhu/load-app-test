cea9c3a4160c995a1d41d459ad2521d15a4c1ef3
JEREMIA-SUNNY <you@example.com>
2024-02-16 13:21:38 +0530

    the table elements

--- src/pages/test.dev.tsx
+++ src/pages/test.dev.tsx
@@ -1,4 +1,5 @@
 import AddIcon from "~/_features/layout/navbar/_components/AddButton";
+import DataTable from "~/_features/layout/navbar/_components/DataTable";
 import EditIcon from "~/_features/layout/navbar/_components/EditButton";
 import SignOut from "~/_features/layout/navbar/_components/SignOutButton";
 
@@ -6,8 +7,7 @@ const Dev = () => {
   return (
     <div className="h-screen">
       <div className=" flex h-full items-center  justify-center gap-5">
-        <AddIcon toolTip="Add" /> <SignOut toolTip="Sign Out" />{" "}
-        <EditIcon toolTip="Edit" />
+        <DataTable />
       </div>
     </div>
   );
